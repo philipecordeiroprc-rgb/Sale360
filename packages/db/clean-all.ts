aeb8b24d629db70efbe8093e28d7bdf286488142
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-05-17 19:26:10 -0300

    auto: update clean-all.ts

--- packages/db/clean-all.ts
+++ packages/db/clean-all.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@sale360/db';
 
 async function cleanAll() {
   console.log('🧹 Limpando todos os dados...\n');
