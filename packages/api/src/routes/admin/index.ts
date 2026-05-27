27fd2bbc2ace61fbcc4b5157817fa8d820e1b3c5
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-05-26 22:42:30 -0300

    auto: update index.ts

--- packages/api/src/routes/admin/index.ts
+++ packages/api/src/routes/admin/index.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginAsync } from 'fastify';
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
-import { prisma } from '@sale360/db';
+import { prisma, Prisma } from '@sale360/db';
 import { sendResetEmail } from '../../services/email.js';
 import { z } from 'zod';
 
