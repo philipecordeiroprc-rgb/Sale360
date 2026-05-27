43bf633ff27f98a02f8edb4f8b3e4840a043484e
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-05-26 22:29:03 -0300

    auto: update index.ts

--- packages/api/src/routes/auth/index.ts
+++ packages/api/src/routes/auth/index.ts
@@ -5,7 +5,6 @@ import { prisma } from '@sale360/db';
 import { generateToken, generateRefreshToken, generateTwoFactorToken, verifyTwoFactorToken, getJwtSecret } from '../../middleware/auth.js';
 import { sendResetEmail } from '../../services/email.js';
 import { authenticator } from 'otplib';
-import bcryptTs from 'bcrypt';
 import { z } from 'zod';
 
 const loginSchema = z.object({
