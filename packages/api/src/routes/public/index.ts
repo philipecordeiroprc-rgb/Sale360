bc218a55b301774550962ab511592dbf86036327
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-05-28 00:00:12 -0300

    auto: update index.ts

--- packages/api/src/routes/public/index.ts
+++ packages/api/src/routes/public/index.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsync } from 'fastify';
 import { prisma } from '@sale360/db';
 import { z } from 'zod';
+import { normalizePaymentMethod, hasFiadoPayment } from '../../lib/payment-utils';
 
 const publicOrderSchema = z.object({
   tenantSlug: z.string().min(1),
