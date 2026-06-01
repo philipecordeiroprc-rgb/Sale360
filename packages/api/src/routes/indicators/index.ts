43ab64a09f5fb58e24590b403751bb5cf4e7e1e9
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-06-01 07:54:04 -0300

    auto: update index.ts

--- packages/api/src/routes/indicators/index.ts
+++ packages/api/src/routes/indicators/index.ts
@@ -11,14 +11,8 @@ export const indicatorRoutes: FastifyPluginAsync = async (app) => {
 
     // ── Date filter ──
     const dateFilter: any = {};
-    if (startDate) {
-      const [y, m, d] = startDate.split('-').map(Number);
-      dateFilter.gte = new Date(y, m - 1, d);
-    }
-    if (endDate) {
-      const [y, m, d] = endDate.split('-').map(Number);
-      dateFilter.lte = new Date(y, m - 1, d, 23, 59, 59, 999);
-    }
+    if (startDate) dateFilter.gte = startOfDay(startDate);
+    if (endDate) dateFilter.lte = endOfDay(endDate);
     const hasDateFilter = Object.keys(dateFilter).length > 0;
 
     const orderWhere: any = {
