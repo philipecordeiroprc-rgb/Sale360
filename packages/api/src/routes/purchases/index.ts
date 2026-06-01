201d50c6ebdc9711ef0ad7412d2e3bb78a454ee5
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-06-01 07:51:41 -0300

    auto: update index.ts

--- packages/api/src/routes/purchases/index.ts
+++ packages/api/src/routes/purchases/index.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsync } from 'fastify';
 import { prisma } from '@sale360/db';
 import { z } from 'zod';
+import { startOfDay, endOfDay } from '../../lib/date-utils.js';
 
 const purchaseItemSchema = z.object({
   productId: z.string().optional(),
