aa4dc7720a0805e223ddecfe1332f007756c0329
philipecordeiroprc-rgb <philipecordeiroprc@gmail.com>
2026-06-01 07:54:03 -0300

    auto: update index.ts

--- packages/api/src/routes/indicators/index.ts
+++ packages/api/src/routes/indicators/index.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsync } from 'fastify';
 import { prisma } from '@sale360/db';
 import { normalizePaymentMethod } from '../../lib/payment-utils.js';
+import { startOfDay, endOfDay } from '../../lib/date-utils.js';
 
 const r2 = (v: number) => Math.round(v * 100) / 100;
 
